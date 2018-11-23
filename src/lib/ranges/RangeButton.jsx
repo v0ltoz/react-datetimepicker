d3c83707a8ccd25336f7243ee6961652008ef417
Christopher McGowan <christopherdavidmcgowan@googlemail.com>
2018-11-23 16:43:42 +0000

    Added ability to tab and up arrow, down arrow through range buttons

--- src/lib/ranges/RangeButton.jsx
+++ src/lib/ranges/RangeButton.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import '../style/DateTimeRange.css'
 import "../style/DateTimeRange.css"
 
@@ -7,21 +8,42 @@ class RangeButton extends React.Component{
         super(props);
         this.state = {
             style : "rangebuttonstyle",
-            focused: false
         }
 
         this.mouseEnter = this.mouseEnter.bind(this);
         this.mouseLeave = this.mouseLeave.bind(this);
         this.onFocus = this.onFocus.bind(this);
         this.onBlur = this.onBlur.bind(this);
+        this.keyDown = this.keyDown.bind(this);
     }
 
     componentWillReceiveProps(nextProps){
+        let focused = nextProps.focused[nextProps.index];
         // If select set to hover style 
-        if(nextProps.index === nextProps.selectedRange){
+        if(nextProps.index === nextProps.selectedRange || focused){
             this.setState({style:"rangeButtonSelectedStyle"})
         }else{
             this.setState({style:"rangebuttonstyle"})
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        let isComponentViewing = this.props.index === this.props.viewingIndex;
+        let focused = this.props.focused;
+        let focusedOnARange = false
+        for(let i = 0; i < focused.length; i++){
+            if(focused[i] === true){
+                focusedOnARange = true;
+                break;
+            }
+        }
+        // If the component we are currently on is the selected viewing component
+        // and we are focused on it according to our focused matrix.
+        // Then add an event listener for this button and set it as focused
+        if(isComponentViewing && focusedOnARange){
+            document.addEventListener("keydown", this.keyDown, false);
+            this.button.tabIndex = 0;
+            this.button.focus();
         }
     }
 
@@ -46,22 +68,53 @@ class RangeButton extends React.Component{
 
     onFocus(){
         this.setState({focused: true});
+        this.props.setFocusedCallback(this.props.index, true);
         this.mouseEnter(true);
     }
 
     onBlur(){
         this.setState({focused: false});
+        this.props.setFocusedCallback(this.props.index, false);
         this.mouseLeave(false);
+        document.removeEventListener("keydown", this.keyDown, false);
+    }
+
+    keyDown(e){
+        let componentFocused =  document.activeElement === ReactDOM.findDOMNode(this.button);
+        // Up Key
+        if (e.keyCode === 38 && componentFocused) {
+            e.preventDefault();
+            this.props.viewingIndexChangeCallback(this.props.index - 1);
+        }
+        // Down Key
+        else if (e.keyCode === 40 && componentFocused) {
+            e.preventDefault();
+            this.props.viewingIndexChangeCallback(this.props.index + 1);
+        }
+        // Space Bar and Enter
+        else if(e.keyCode === 32 || e.keyCode === 13){
+            this.props.rangeSelectedCallback(this.props.index, this.props.label);
+        }
     }
 
     render(){
+        let isViewingIndex = this.props.viewingIndex === this.props.index;
+
+        let tabIndex;
+        if(isViewingIndex){
+            tabIndex = 0;
+        }else{
+            tabIndex = -1;
+        }
         return(
-            <div className={this.state.style} 
+            <div 
+                ref={button => { this.button = button; }}
+                className={this.state.style} 
                 onMouseEnter={this.mouseEnter} 
                 onMouseLeave={this.mouseLeave}
                 onFocus={this.onFocus}
                 onBlur={this.onBlur}
-                tabIndex={0}
+                tabIndex={tabIndex}
             > 
                 <div className="rangebuttontextstyle" onClick={() => this.props.rangeSelectedCallback(this.props.index, this.props.label)}>
                     {this.props.label}
--- src/lib/ranges/Ranges.jsx
+++ src/lib/ranges/Ranges.jsx
@@ -5,6 +5,52 @@ import RangeButton from "./RangeButton"
 
 class Ranges extends React.Component{
 
+    constructor(props){
+        super(props);
+
+        let focused = [];
+        let ranges = Object.values(this.props.ranges)
+        for(let i = 0; i < ranges.length; i++){
+            focused.push(false);
+        }
+
+        this.state = ({
+            viewingIndex: 0,
+            focused : focused
+        })
+
+        this.viewingIndexChangeCallback = this.viewingIndexChangeCallback.bind(this);
+        this.setFocusedCallback = this.setFocusedCallback.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps){
+        // On Change of Selected Range reset viewing index to be the range index
+        if(this.props.selectedRange !== nextProps.selectedRange){
+            this.setState({
+                viewingIndex : nextProps.selectedRange
+            })
+        }
+    }
+
+    viewingIndexChangeCallback(newIndex){
+        // Allow a new item selected to be made
+        let length = this.state.focused.length
+        if(newIndex >= 0 && newIndex < length){
+            this.setState({
+                viewingIndex : newIndex
+            })
+        }
+    }
+
+    setFocusedCallback(index, focusedInput){
+        // Set the focus value of indexed item, focusedInput is true or false
+        let focused = this.state.focused;
+        focused[index] = focusedInput;
+        this.setState({
+            focused : focused
+        })
+    }
+
     render(){
         // Map the range index and object name and value to a range button
         return(
@@ -17,6 +63,10 @@ class Ranges extends React.Component{
                         value={this.props.ranges[range]} 
                         selectedRange={this.props.selectedRange}
                         rangeSelectedCallback={this.props.rangeSelectedCallback}
+                        viewingIndex={this.state.viewingIndex}
+                        viewingIndexChangeCallback={this.viewingIndexChangeCallback}
+                        focused={this.state.focused}
+                        setFocusedCallback={this.setFocusedCallback}
                     />
                 ))}
             </div>
