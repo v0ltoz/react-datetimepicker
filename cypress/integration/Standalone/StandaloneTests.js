d0974fa6f8723f70642e976fa716abb1b6d9e2bf
v0ltoz <christopherdavidmcgowan@googlemail.com>
2019-10-10 15:20:32 +0100

    Added Cypress tests for Standalone functionality

--- cypress/integration/Standalone/StandaloneTests.js
+++ cypress/integration/Standalone/StandaloneTests.js
@@ -1,31 +1,87 @@
 import {
   visitAndClickDateTime,
   keyboardOrClickThenCheckAllChanges,
-  keyboardOrClickThenCheckAllChangesWithin,
+  keyboardOrClickThenCheckAllChangesWithin, checkStyleOfSelectorWithin, assertSelectedDateCorrectBothDatesChange,
 } from '../HelperFunctions';
 
-describe('Date Time Picker Mobile Mode Tests', function() {
-  it('Mobile Mode not disabled (Default) Mobile Mode Active, When Below 680px', function() {
-    visitAndClickDateTime('DateTimeRangeContainerMobileMode');
-    cy.viewport('iphone-5'); // Set viewport to 375px x 667px
-    cy.get('#DateTimeRangeContainerMobileMode').within(() => {
-      cy.get('#daterangepicker').should('have.css', 'display', 'block');
+describe('Standalone Picker Tests', function() {
+  it('Is standalone picker showing by default', function() {
+    cy.visit('http://localhost:3000/');
+    cy.scrollTo('bottom');
+    cy.get('#DateTimeRangeContainerStandalone').within(() => {
+      cy.get('#rangeButton0').should('be.visible');
+      cy.get('#DateTimeInput_end').should('be.visible');
     });
   });
 
-  it('Mobile Mode not showing, When above 680px', function() {
-    visitAndClickDateTime('DateTimeRangeContainerMobileMode');
-    cy.viewport('macbook-11');
-    cy.get('#DateTimeRangeContainerMobileMode').within(() => {
-      cy.get('#daterangepicker').should('have.css', 'display', 'flex');
+  it('Standalone picker allows date changes', function() {
+    cy.get('#DateTimeRangeContainerStandalone').within(() => {
+      cy.get('#DateTimeInput_start')
+        .clear()
+        .type('21-09-2016 23:59')
+        .blur();
+      cy.get('#DateTimeInput_end')
+        .clear()
+        .type('25-09-2016 00:00')
+        .blur();
+      cy.get('#startNotifierID').contains('From Date');
+      cy.get('#endNotifierID').contains('To Date');
+    });
+    checkStyleOfSelectorWithin(
+      'DateTimeRangeContainerStandalone',
+      'row_3_cell_2_end',
+      'color',
+      'rgb(255, 255, 255)',
+    );
+    keyboardOrClickThenCheckAllChangesWithin(
+      'DateTimeRangeContainerStandalone',
+      'row_3_cell_1_end',
+      'row_3_cell_1_end',
+      'click',
+      '23',
+      '59',
+      'September',
+      '2016',
+      '0',
+      '0',
+      'September',
+      '2016',
+      'row_1_cell_0_start',
+      'row_3_cell_5_end',
+      'row_3_cell_2_start',
+      'row_3_cell_6_end',
+      '21-09-2016 23:59',
+      '25-09-2016 00:00',
+    );
+    cy.get('#DateTimeRangeContainerStandalone').within(() => {
+      cy.get('#startNotifierID').contains('From Date');
+      cy.get('#endNotifierID').contains('To Date');
     });
   });
 
-  it('Mobile Mode forced, When above 680px when force prop set', function() {
-    visitAndClickDateTime('DateTimeRangeContainerForceMobileMode');
-    cy.viewport('macbook-11'); // Set viewport to 375px x 667px
-    cy.get('#DateTimeRangeContainerForceMobileMode').within(() => {
-      cy.get('#daterangepicker').should('have.css', 'display', 'block');
+  it('Standalone picker can click on Ranges', function() {
+    cy.get('#DateTimeRangeContainerStandalone').within(() => {
+      cy.get('#rangeButton7').click();
     });
+
+    assertSelectedDateCorrectBothDatesChange(
+      'DateTimeRangeContainerStandalone',
+      0,
+      0,
+      'August',
+      2018,
+      0,
+      0,
+      'August',
+      2018,
+      'row_1_cell_0_start',
+      'row_1_cell_0_start',
+      'row_0_cell_2_start',
+      'row_0_cell_3_end',
+      '01-08-2018 00:00',
+      '02-08-2018 00:00'
+      ,
+    );
   });
+
 });
