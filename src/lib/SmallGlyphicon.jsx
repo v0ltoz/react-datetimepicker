95be700c5c01ada31323447b0b8ed9947ac67370
Nicola Tomasino <nicola.tomasino@kft.it>
2020-01-08 12:19:19 +0100

    fixed arrows to change month not working

--- src/lib/SmallGlyphicon.jsx
+++ src/lib/SmallGlyphicon.jsx
@@ -1,8 +1,17 @@
-import React, { Component } from 'react';
-import './SmallGlyphicon.css';
+import React, { Component } from "react";
+import "./SmallGlyphicon.css";
 
 export default class SmallGlyphicon extends Component {
-	render() {
-		return <div className={'glyphicon glyphicon-' + this.props.glyph + (this.props.className != null ? ' '+this.props.className:'')} />;
-	}
+  render() {
+    return (
+      <div
+        className={
+          "glyphicon glyphicon-" +
+          this.props.glyph +
+          (this.props.className != null ? " " + this.props.className : "")
+        }
+        onClick={this.props.onClickHandler}
+      />
+    );
+  }
 }
