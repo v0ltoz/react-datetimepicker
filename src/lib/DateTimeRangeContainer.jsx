54dee98c4fbf1990784874690d871dd22ec633d9
Kai Zhu <zhuk2@mcmaster.ca>
2022-05-05 11:58:39 -0400

    replaced findDomNode

--- src/lib/DateTimeRangeContainer.jsx
+++ src/lib/DateTimeRangeContainer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { findDOMNode } from 'react-dom';
 import './style/DateTimeRange.css';
 import PropTypes from 'prop-types';
 import momentPropTypes from 'react-moment-proptypes';
@@ -50,7 +49,7 @@ class DateTimeRangeContainer extends React.Component {
   }
 
   resize() {
-    const domNode = findDOMNode(this).children[0];
+    const domNode = this.container.children[0];
     const mobileModeActive = !this.props.noMobileMode; // If no mobile mode prop not set then allow mobile mode
     const mobileModeForce = this.props.forceMobileMode; // If force mobile mode prop is set then force mobile mode
     let boundingClientRect = domNode.getBoundingClientRect();
@@ -73,14 +72,14 @@ class DateTimeRangeContainer extends React.Component {
         screenWidthToTheRight: widthRightOfThis,
         containerClassName: 'daterangepicker daterangepickerleft',
       });
-    } else if(this.props.centerMode){
+    } else if (this.props.centerMode) {
       this.setState({
         x: boundingClientRect.height + 5,
         y: -440,
         screenWidthToTheRight: widthRightOfThis,
         containerClassName: 'daterangepicker daterangepickerleft',
       });
-    }else {
+    } else {
       this.setState({
         x: boundingClientRect.height + 5,
         y: 0,
@@ -220,7 +219,7 @@ DateTimeRangeContainer.propTypes = {
   leftMode: PropTypes.bool,
   centerMode: PropTypes.bool,
   standalone: PropTypes.bool,
-  twelveHoursClock: PropTypes.bool
+  twelveHoursClock: PropTypes.bool,
 };
 
 export default DateTimeRangeContainer;
