d27081a7faf50f704e32b388acc892ad7cdad1f0
Mohsen Taleb <mohsentaleb@gmail.com>
2023-04-25 09:52:29 +0300

    Make build system for the lib.

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
@@ -7,5 +8,27 @@ export default defineConfig({
   plugins: [react(), svgr()],
   server: {
     port: 3000,
-  }
+  },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, './src/lib/index.jsx'),
+      // entry: './src/lib/index.js',
+      name: 'DateTimeRange',
+      // the proper extensions will be added
+      fileName: 'react-datepicker',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'React',
+        },
+      },
+    },
+  },
 })
