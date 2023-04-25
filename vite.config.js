975a0b5f5f4205d3c37106a74ca11cb363f9a930
Mohsen Taleb <mohsentaleb@gmail.com>
2023-04-25 14:55:06 +0300

    Adjust package name in build script.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import svgr from 'vite-plugin-svgr'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,13 +10,12 @@ export default defineConfig({
     port: 3000,
   },
   build: {
+    copyPublicDir: false,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, './src/lib/index.jsx'),
       // entry: './src/lib/index.js',
       name: 'DateTimeRange',
-      // the proper extensions will be added
-      fileName: 'react-datepicker',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
@@ -31,4 +30,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
